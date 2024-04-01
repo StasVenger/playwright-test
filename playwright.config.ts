134ba5f1d89239772857b7a361160968c1d2df2f
StasVenger <stasvenger1@gmail.com>
2024-04-02 04:45:34 +0600

    Allure integration

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,16 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [
+    [
+      "allure-playwright",
+      {
+        detail: true,
+        outputFolder: "allure-results",
+        suiteTitle: false,
+      },
+    ],
+  ],
   use: {
     baseURL: 'https://dzen.ru/'
   },
